3d85249b16180719f6342763f26be61499d78452
Shubham <Singhalshubham610@gmail.com>
2023-02-01 23:36:20 +0530

    greeting code

---
+++ src/GreetingMessage.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class GreetingMessage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // used to take input
+        String str = sc.nextLine();
+        System.out.println("Happy Birthday "+ str);
+    }
+}
