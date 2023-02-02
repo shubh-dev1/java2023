439a9e2e2e887279f4a3a5840a6167377ba1a90a
Shubham <Singhalshubham610@gmail.com>
2023-02-02 23:57:46 +0530

    change rupee to Usd

---
+++ src/RupeesToUsd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class RupeesToUsd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Rupee :");
+        float rupee = sc.nextFloat();
+        float usd;
+
+        usd = rupee/83;
+
+        System.out.println("$"+ usd);
+    }
+}
