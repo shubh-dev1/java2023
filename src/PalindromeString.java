6b30c7d01d774f6a2a7e175f30daad66216a9d3f
Shubham <Singhalshubham610@gmail.com>
2023-02-04 23:49:07 +0530

    check whether string is a palindrome or not

---
+++ src/PalindromeString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String dummy = "";
+        for(int i =str.length()-1;i>=0;i--){
+            dummy += str.charAt(i);
+        }
+        if(str.equals(dummy)){
+            System.out.println("String is a palindrome");
+        }else{
+            System.out.println("String is not a palindrome");
+        }
+    }
+}
