4103d7f4b744edf70031db79bb338f030ba6b29d
Shubham <Singhalshubham610@gmail.com>
2023-07-03 23:43:11 +0530

    put in basic package

---
+++ src/Pattern/EightPattern.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class EightPattern {
+  public static void main(String args[]){
+    //1
+       String a  = "shubham";   // next   s h u b h a m
+
+       String b = "shubham singhal"; // next line
+
+//        System.out.print(a.charAt(3));
+    System.out.println(b.charAt(6));
+  }
+}
